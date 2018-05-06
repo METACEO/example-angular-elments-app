b2ac93a645800cd12c95e35f6d5b23a6740eb248
James Allen <james.allen@meta.ceo>
2018-05-06 17:04:28 -0500

    Adding the angular elements loader and the angular element js sources.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {Component, Inject} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  public elements: string[] = [
+    'example-angular-elements-component-box',
+    'example-angular-elements-component-button'
+  ];
+  public scripts: string[] = [];
+
+  constructor(@Inject(DOCUMENT) public document) {
+  }
+
+  public load(element: string) {
+    this.loadScript(element)
+      .then(() => {
+        const $elements = document.getElementById('elements');
+        const $element = document.createElement(element);
+        $elements.appendChild($element);
+      });
+  }
+
+  public loadScript(element: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      // If we've already loaded the angular element js script the return immediately.
+      if (this.scripts[element] >= 0) {
+        return resolve();
+      }
+      // Otherwise, signify that we are loading the angular element js script.
+      this.scripts[element] = true;
+      // Load the angular element js script.
+      const $script = this.document.createElement('script');
+      $script.src = `/assets/elements/${element}.js`;
+      $script.onload = () => resolve();
+      this.document.head.appendChild($script);
+    });
+  }
 }
